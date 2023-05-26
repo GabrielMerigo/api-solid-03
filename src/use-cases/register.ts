2178604ac9fdda69a32782c4fcf8b5a23ced245b
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-26 18:37:50 -0300

    solidpriciples

---
+++ src/use-cases/register.ts
@@ -0,0 +1,33 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repositories";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const registerUseCase = async ({
+  name,
+  email,
+  password,
+}: RegisterUseCaseParams) => {
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) throw new Error("E-mail already exists.");
+
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  });
+};
