b0a9dd5567e6a29b0f863ac28bae8d37b9669408
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-05-31 16:51:40 -0300

    more accs

---
+++ src/use-cases/create-gyms/create-gyms.ts
@@ -0,0 +1,34 @@
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { Gym } from "@prisma/client";
+
+interface CreateGymUseCaseCaseParams {
+  title: string;
+  description: string | null; // You create without description
+  phone: string | null;
+  latitude: string;
+  longitude: string;
+}
+
+interface CreateGymUseCaseCaseResponse {
+  gym: Gym;
+}
+
+export class CreateGymUseCaseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    description,
+    phone,
+    latitude,
+    longitude,
+    title,
+  }: CreateGymUseCaseCaseParams): Promise<CreateGymUseCaseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return { user };
+  }
+}
